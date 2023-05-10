e41bfc3d3c5ef5ddb3673a0207ac5df49d77a0d7
kaua <kauasanttosgestoria@gmail.com>
2023-05-10 11:49:16 -0300

    CONFIG DELETE CASCADE

--- knexfile.js
+++ knexfile.js
@@ -5,11 +5,14 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, "src", "database", "database.db")
-    },
-    migrations: {
-      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
-    },
-    useNullAsDefault: true
+    filename: path.resolve(__dirname, "src", "database", "database.db")
+  },
+  pool: {
+    afterCreate: (conn, cb) => conn.run("PRAGMA foreign_key = ON", cb)
+  },
+  migrations: {
+    directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+  },
+  useNullAsDefault: true
   }
 };
