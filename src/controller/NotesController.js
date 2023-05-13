3ce77ea615b0f0377099e90f95265f639bb38d01
kaua <kauasanttosgestoria@gmail.com>
2023-05-13 13:50:35 -0300

    APPLICATION INNERJOIN

--- src/controller/NotesController.js
+++ src/controller/NotesController.js
@@ -56,10 +56,29 @@ class NotesController{
    }
 
    async index(request, response){
-      const { user_id, title } = request.query;
-      const notes = await knex("notes")
-         .where({ user_id }).orderBy("title")
-         .whereLike("title", `%${title}%`)
+      const { user_id, title, tags } = request.query;
+      let notes;
+
+      if(tags){
+         const filterTags = tags.split(',').map(tag => tag.trim())
+
+         notes = await knex('tags')
+            .select([
+               "notes.id",
+               "notes.title",
+               "notes.user_id"
+            ])
+            .where("notes.user_id", user_id)
+            .whereLike("notes.title", `%${title}%`)
+            .whereIn("name", filterTags)
+            .innerJoin("notes", "notes.id", "tags.note_id")
+            .orderBy("notes.title")
+      } else {
+         notes = await knex("notes")
+            .where({ user_id }).orderBy("title")
+            .whereLike("title", `%${title}%`)
+      }
+
          return response.json(notes)
    };
 
